78454c4b557e1915666e78d7ebe0ec3a3322f96c
iseltarek <iseltarek15@gmail.com>
2023-07-13 12:12:45 -0700

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
-#include<stdio.h>
+#include <stdio.h>
+
 /**
  * main - Entry point
- * 
+ *
  * Description: A C program that prints with put function
  *
  * Return: Always 0 (success)
@@ -9,6 +10,6 @@
 
 int main(void)
 {
- 	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
