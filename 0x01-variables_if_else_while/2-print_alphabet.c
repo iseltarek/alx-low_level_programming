b6b261701c32ce11f6382c150816627e8c215ba8
iseltarek <iseltarek15@gmail.com>
2023-07-14 14:05:46 -0700

    a

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print alphabet using putchar function. 
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(0) 
+{
+	for (char i = 'a'; i <= 'z'; i++)
+	putchar(i);
+	return (0);
+}
