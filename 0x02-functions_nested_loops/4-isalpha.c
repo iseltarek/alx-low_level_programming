a9bff371336f0a056d28dba651b2b7466d82931e
iseltarek <iseltarek15@gmail.com>
2023-07-18 15:51:55 -0700

    a

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -10,7 +10,9 @@
 
 int _isalpha(int c)
 {
-	if ((c >= 96) && (c <= 122) || (c >= 65) && (c <= 87))
+	if (c >= 96 && c <= 122)
+		return(1);
+	if (c >= 65 && c <= 87)
 		return (1);
 	return (0);
 }
