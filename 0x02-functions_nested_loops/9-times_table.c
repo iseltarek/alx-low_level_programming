5ebd51a8095d8e1530a5da0854c077db6ae03b81
iseltarek <iseltarek15@gmail.com>
2023-07-18 17:54:46 -0700

    a

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,20 +7,22 @@
 
 void times_table(void)
 {
-	int i, j;
+	int i, j, pro;
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j <= 9; j++)
+		_putchar(48);
+		for (j = 1; j <= 9; j++)
 		{
-			int pro = i * j;
-
-			_putchar(pro + 48);
-			if (j < 9)
-			{
-				_putchar(',');
+			_putchar(',');
+			_putchar(' ');
+			pro = i * j;
+			if (pro <= 9)
 				_putchar(' ');
-			}
+			else
+				_putchar((pro / 10) + 48);
+		
+			_putchar((pro % 10) + 48);
 		}
 		_putchar('\n');
 	}
