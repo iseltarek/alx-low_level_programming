2d540e162330fae4807501b3e235949124546c3c
iseltarek <iseltarek15@gmail.com>
2023-07-18 17:42:15 -0700

    a

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table
+ *
+*/
+
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			int pro = i * j;
+
+			_putchar(pro + 48);
+			if (j < 9)
+				_putchar(', ');
+		}
+		_putchar('\n');
+	}
+}
