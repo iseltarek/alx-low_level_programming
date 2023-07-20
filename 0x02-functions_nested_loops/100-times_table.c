d44817117acdc80f1f14b312a3f9b1f058b6520f
iseltarek <iseltarek15@gmail.com>
2023-07-19 18:47:40 -0700

    a

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints
+ *
+ * @n: takes number
+*/
+
+void print_times_table(int n)
+{
+	if (n <= 15 || n > 0)
+	{
+		int i, j, pro;
+
+		for (i = 0; i <= n; ++i)
+		{
+			_putchar(48);
+			for (j = 1; j <= n; ++j)
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				pro = i * j;
+				if (pro <= 9)
+					_putchar(' ');
+				if (pro <= 99)
+					_putchar(' ');
+				if (pro >= 100)
+				{
+					_putchar((pro / 100) + 48);
+					_putchar((pro % 10) + 48);
+				}
+				else if (pro <= 99 && pro >= 10)
+					_putchar((pro / 10) + 48);
+				_putchar((pro % 10) + 48);
+			}
+			_putchar('\n');
+		}
+	}
+}
