aa914b6da97adc5d7e46b002f2dcd3c7255e9ea2
iseltarek <iseltarek15@gmail.com>
2023-07-24 12:37:01 -0700

    a

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * reser_to_98 - takes a pointer and update it to point to 98
+ * reset_to_98 - takes a pointer and update it to point to 98
  *
  * @n: int parameter
  *
  * Return: nothing
 */
 
-void reser_to_98(int *n)
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,4 +6,4 @@
 
 void reset_to_98(int *n);
 
-#endif 
+#endif /* MAIN_H */ 
