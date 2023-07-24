3763c175e9135b8c1b7802f31fbb2fd39a857e70
iseltarek <iseltarek15@gmail.com>
2023-07-24 15:02:46 -0700

    a

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_array - prints array
+ *
+ * @a: array parameter
+ * @n: second parameter
+ *
+ * Return: nothing
+*/
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		putchar(a[i]);
+		if (i != n - 1)
+		{
+			putchar(",");
+			putchar(" ");
+		}
+	}
+	putchar('\n');
+}
