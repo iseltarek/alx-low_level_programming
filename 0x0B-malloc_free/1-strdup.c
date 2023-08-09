2d5229d9ada224281834a87a3559cad52c7ea453
iseltarek <iseltarek15@gmail.com>
2023-08-09 10:11:33 -0700

    a

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * *_strdup - function
+ * @str: pointer
+ * Return: m
+*/
+char *_strdup(char *str)
+{
+	int s = 0, i;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[s])
+		s++;
+	char *m = malloc(s * sizeof(*str) + 1);
+
+	if (m == 0)
+		return (NULL);
+	else
+	{
+		for (i = 0; i < s; i++)
+			m[i] = str[i];
+	}
+	return (m);
+}
