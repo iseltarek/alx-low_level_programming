b483b9a15d91c25499b33aa731be96dd3f676ed0
iseltarek <iseltarek15@gmail.com>
2023-08-09 16:39:32 -0700

    A'

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -32,10 +32,10 @@ char *argstostr(int ac, char **av)
 		return (NULL);
 	for (i = 0; i < ac; i++)
 	{
-		for (j = 0; av[i][j][i] != '\0'; j++, cmpt++)
+		for (j = 0; av[i][j] != '\0'; j++, cmpt++)
 			s[cmpt] = av[i][j];
 		s[cmpt] = '\n';
-		cmpt++
+		cmpt++;
 	}
 	s[cmpt] = '\0';
 	return (s);
