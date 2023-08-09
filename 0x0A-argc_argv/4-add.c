a8521384fff821d6927a61e456d23fb6b311be0d
iseltarek <iseltarek15@gmail.com>
2023-08-09 09:03:24 -0700

    a

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,17 +11,16 @@ int main(int argc, char *argv[])
 	int i, sum = 0;
 	char *c;
 
-	for (i = 1; i < argc; i++)
+	while (--argc)
 	{
-		for (c = argv[i]; *c; c++)
+		for (c = argv[argc]; *c; c++)
 		{
 			if (*c < '0' || *c > '9')
 			{
-				printf("Error\n");
-				return (1);
+				return (printf("Error\n"), 1);
 			}
 		}
-		sum += atoi(argv[i]);
+		sum += atoi(argv[argc]);
 	}
 	printf("%d\n", sum);
 	return (0);
