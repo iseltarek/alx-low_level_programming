95bf780acd23d7ca5836bff527fab1780813032a
iseltarek <iseltarek15@gmail.com>
2023-08-18 09:37:23 -0700

    a

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+/**
+ * int_index - searches for int
+ * @array: array
+ * @size: size
+ * @cmp:function
+ * Return: integer
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array && cmp && size)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]))
+			{
+				x = i;
+				break;
+			}
+		}
+		return (x);
+	}
+	return (-1);
+}
