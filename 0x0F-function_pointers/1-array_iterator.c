c481a6ff4681f4a800cabd2783bbee17cf7ec765
iseltarek <iseltarek15@gmail.com>
2023-08-18 09:08:46 -0700

    a

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - maps an array through a func
+ * @array:int array
+ * @size: size of array
+ * @action: finction
+ * Return: nothing
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (array && size && action)
+	{
+		for (i = 0; i < size; i++)
+			action(array[i]);
+	}
+}
