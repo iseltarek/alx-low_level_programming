9f71f42af548b7695cd8f766506a7e1add49d1a3
iseltarek <iseltarek15@gmail.com>
2023-08-19 07:48:22 -0700

    A

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -8,8 +8,9 @@
 */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int *end = &array + size - 1;
+	int *end;
 
+	end = &array + size - 1;
 	if (array && size && action)
 	{
 		while (array <= end)
