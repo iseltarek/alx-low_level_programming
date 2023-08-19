65aeb29288eb38730b655c8ed238eb617304d12c
iseltarek <iseltarek15@gmail.com>
2023-08-19 08:10:51 -0700

    a

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include "dog.h"
 /**
- * init_dog - initializes 
+ * init_dog - initialize
  * @d: init
  * @name: dogs name
  * @age: age
- * @owner:
- * @dogs: owner name
+ * @owner: owner name
  * Return: void
 */
 void init_dog(struct dog *d, char *name, float age, char *owner)
