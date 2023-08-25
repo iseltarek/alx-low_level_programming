78e46722c5ce995d1fe45c5d56a1cbae1b704b95
iseltarek <iseltarek15@gmail.com>
2023-08-25 16:51:55 -0700

    a

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -25,7 +25,10 @@ size_t print_list(const list_t *h)
 
 	while (h)
 	{
-		printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
 		i++;
 	}
--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -4,13 +4,13 @@
  * @h: pointer to node
  * Return:size
 */
-size_t list_len(list_t *h)
+size_t list_len(const list_t *h)
 {
 	size_t i = 0;
 
 	while (h)
 	{
-		h = h->next;
+		h = h.next;
 		i++;
 	}
 	return (i);
