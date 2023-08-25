65a167ba5714a3e9debcf40501ea113f7ddc8f4f
iseltarek <iseltarek15@gmail.com>
2023-08-25 16:40:04 -0700

    a

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * list_len - lenght
+ * @h: pointer to node
+ * Return:size
+*/
+size_t list_len(list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
