bff33e2d041c1ed1cdf51373076737ab172781e2
iseltarek <iseltarek15@gmail.com>
2023-08-25 17:22:38 -0700

    a

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * *add_node - lists
+ * @head: addres
+ * @str: str field
+ * Return: size of list
+*/
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *t;
+	int len = 0;
+
+	t = malloc(sizeof(list_t));
+	if ( t == NULL)
+		return (NULL);
+	while (str[len])
+		len++;
+	t->len = len;
+	t->str = strdup(str);
+	t->next = *head;
+	*head = t;
+	return (t);
+}
