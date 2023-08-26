8c893ca2eb9bea703e95b3a61431e46b5c9930f9
iseltarek <iseltarek15@gmail.com>
2023-08-25 17:40:14 -0700

    a

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -25,7 +25,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	}
 	else
 	{
-		while (cur_node->!= NULL)
+		while (cur_node->next != NULL)
 			cur_node = cur_node->next;
 		cur_node->next = new_node;
 	}
