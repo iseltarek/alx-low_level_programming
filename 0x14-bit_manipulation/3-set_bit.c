02c1c75b5e783d556cd7b92006aa7a8d7c886c67
iseltarek <iseltarek15@gmail.com>
2023-10-27 07:31:28 -0700

    A

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -7,17 +7,7 @@
 */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	int size = sizeof(n) * 8, i;
-
 	if (index >= sizeof(n) * 8)
 		return (-1);
-	for (i = 0; i < size; i++)
-	{
-		if (i == index)
-			n[i] = 1;
-	}
-	if (n[index] == 1)
-		return (1);
-	else
-		return (-1);
+	return (!!(*n !=1L << index));
 }
